3469646b1f80763cc35d98491276484f19ae51c8
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-05-09 02:47:59 +1000

    created registration and signup form and passed into root address

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import path from "path";
+import fs from "fs";
+
+const _dirname = path.resolve();
 
 const app = express();
-app.use(express.json()); ///injecting the middleware between the server and client, power to manipulate the data in between
+app.use(express.json());
+app.use(express.urlencoded()); //encoding the json file
+///injecting the middleware between the server and client, power to manipulate the data in betwee
+
+app.get("/", (req, res) => {
+  res.sendFile(_dirname + "/forms/loginForm.html");
+});
 
 app.listen(8000, (error) => {
   console.log("http://localhost:8000");
